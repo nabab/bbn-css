9b5129a4788ebedeb38c00f4aee9bbcd9a3eb516
nabab <thomas.nabet@gmail.com>
2023-10-23 12:10:51 +0200

    Latest changes

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,10 @@ const rename = require('gulp-rename');
 gulp.task('compile-less', function() {
   return gulp.src('./src/styles/*.less') // Source LESS files
     .pipe(less()) // Compile LESS to CSS
-    .pipe(gulp.dest('./dist')) // Save CSS to destination folder
+    .pipe(gulp.dest('./dist/css')) // Save CSS to destination folder
     .pipe(cleanCSS()) // Minify the CSS
     .pipe(rename({ suffix: '.min' })) // Rename to .min.css
-    .pipe(gulp.dest('./dist')); // Save minified CSS to destination folder
+    .pipe(gulp.dest('./dist/css')); // Save minified CSS to destination folder
 });
 
 // Default Gulp task
