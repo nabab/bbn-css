2c2f2c543f0e282774d7a524e8b1af69b44e71cc
nabab <thomas.nabet@gmail.com>
2025-07-29 16:28:40 +0200

    Latest changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@ const gulp = require('gulp');
 const less = require('gulp-less');
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
+const replace = require('gulp-replace');
 
 // Compile LESS and save as .css
 gulp.task('compile-less', function() {
   return gulp.src('./src/styles/*.less') // Source LESS files
     .pipe(less()) // Compile LESS to CSS
+    .pipe(replace('url(../css/', 'url(../../fonts/')) // Replace URL paths
     .pipe(gulp.dest('./dist/css')) // Save CSS to destination folder
     .pipe(cleanCSS()) // Minify the CSS
     .pipe(rename({ suffix: '.min' })) // Rename to .min.css
